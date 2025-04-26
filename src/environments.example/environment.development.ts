ba19a636e2d4d80f47fcadd34908eab475d734c8
oqrosha <91896457+oqrosha@users.noreply.github.com>
2025-04-27 01:29:28 +0400

    Fixed and added forgotten changes in environment files

--- src/environments.example/environment.development.ts
+++ src/environments.example/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'YOUR_API_URL',
-  hubUrl: 'YOUR_HUB_URL',
+  apiUrl: 'YOUR_DEVELOPMENT_API_URL',
+  hubUrl: 'YOUR_DEVELOPMENT_HUB_URL',
 };
--- src/environments.example/environment.ts
+++ src/environments.example/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://localhost:7056/api/', // need to be changed into valid hosted back-end url
-  hubUrl: 'https://localhost:7056/', // same here
+  apiUrl: 'YOUR_PRODUCTION_API_URL',
+  hubUrl: 'YOUR_PRODUCTION_HUB_URL',
 };
